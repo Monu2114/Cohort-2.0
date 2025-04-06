6c99563d4d269cc18c4f428891a47e077c325b37
Monu_2114 <monu.nanabala@gmail.com>
2025-04-06 16:48:26 +0530

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,25 @@
 */
 
 function isPalindrome(str) {
+  // str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  // let n = str.length;
+  // for (let i = 0; i < n / 2; i++) {
+  //   if (str.charAt(i) != str.charAt(n - i - 1)) return false;
+  // }
+  // return true;
+  // two pointer approach -
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    while (left < right && !isAlphaNumeric(str[left])) left++;
+    while (left < right && !isAlphaNumeric(str[right])) right--;
+    if (str[left].toLowerCase() !== str[right].toLowerCase()) return false;
+    left++;
+    right--;
+  }
   return true;
 }
-
+function isAlphaNumeric(char) {
+  return /^[a-z0-9]$/i.test(char); // matches a-z or 0-9, case-insensitive
+}
 module.exports = isPalindrome;
