30380b01f94c535a3d42b1c74453863f4dd2f7d9
Monu_2114 <monu.nanabala@gmail.com>
2025-04-06 15:49:17 +0530

    expenditure --> did it on own !!!

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,70 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  // return list with objects containing category and price spent
+  // const output = [];
+  const arr = [];
+  // transactions.map((obj) => {
+  //   output.push({ category: obj.category, totalSpent: obj.price });
+  // });
+  // for (let i = 0; i < transactions.length; i++) {
+  //   let index = arr.findIndex(
+  //     (item) => item.category === transactions[i].category
+  //   );
+  //   if (index == -1) {
+  //     arr.push({
+  //       category: transactions[i].category,
+  //       totalSpent: transactions[i].price,
+  //     });
+  //   } else {
+  //     arr[index].totalSpent = arr[index]["totalSpent"] + transactions[i].price;
+  //   }
+  // }
+  transactions.forEach((element) => {
+    let index = arr.findIndex((item) => item.category == element.category);
+    if (index == -1) {
+      arr.push({ category: element.category, totalSpent: element.price });
+    } else arr[index].totalSpent += element.price;
+  });
+  console.log(arr);
 
+  return arr;
+}
+calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: "Food",
+    itemName: "Burger",
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: "Clothing",
+    itemName: "T-Shirt",
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: "Electronics",
+    itemName: "Headphones",
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: "Clothing",
+    itemName: "Jeans",
+  },
+]);
 module.exports = calculateTotalSpentByCategory;
